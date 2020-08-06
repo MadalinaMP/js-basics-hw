4481a699c5536a3abc1fafabad1802a14d3e6fd2
MadalinaMP <madalinapuimp@gmail.com>
2020-08-06 16:10:11 +0300

    solved 3

--- control-structures/controlStructuresApp.js
+++ control-structures/controlStructuresApp.js
@@ -11,3 +11,13 @@ for (var index = 1; index <= 20; index++) {
         console.log(index);
     }
 }
+
+console.log('3. compute the sum of the elements of an array and display it in the console')
+
+var list = [3, 1, 2, 3, 15, 24, 55, 3, 3, 3];
+var totalSum = 0;
+for (var index = 0; index < list.length; index++) {
+    currentElement = list[index];
+    totalSum = totalSum + currentElement;
+}
+console.log('The sum of the array: ' + list + ' is: ' + totalSum);
