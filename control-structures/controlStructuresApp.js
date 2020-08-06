4b2a67afc51503d4096c8cba1eb18aaadab1e041
MadalinaMP <madalinapuimp@gmail.com>
2020-08-06 15:51:13 +0300

    solved 1

--- control-structures/controlStructuresApp.js
+++ control-structures/controlStructuresApp.js
@@ -1,4 +1,4 @@
-//1. display in the console the numbers from 1 to 20
+console.log('1. display in the console the numbers from 1 to 20');
 
 for (var index = 1; index <= 20; index++) {
     console.log(index);
