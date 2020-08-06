9eda7ad0d7d7f5b60836d64cc4db7849de5507a2
MadalinaMP <madalinapuimp@gmail.com>
2020-08-06 16:19:58 +0300

    solved 4

--- control-structures/controlStructuresApp.js
+++ control-structures/controlStructuresApp.js
@@ -12,7 +12,7 @@ for (var index = 1; index <= 20; index++) {
     }
 }
 
-console.log('3. compute the sum of the elements of an array and display it in the console')
+console.log('3. compute the sum of the elements of an array and display it in the console');
 
 var list = [3, 1, 2, 3, 15, 24, 55, 3, 3, 3];
 var totalSum = 0;
@@ -21,3 +21,15 @@ for (var index = 0; index < list.length; index++) {
     totalSum = totalSum + currentElement;
 }
 console.log('The sum of the array: ' + list + ' is: ' + totalSum);
+
+console.log('4. compute the maximum of the elements of an array and display it in the console ');
+
+var list = [3, 1, 2, 3, 15, 24, 55, 3, 3, 3];
+currentMax = list[0];
+for (var index = 0; index <= list.length; index++) {
+    currentElement = list[index];
+    if (currentElement > currentMax) {
+        currentMax = currentElement;
+    }
+}
+console.log('The max of the array: ' + list + ' is: ' + currentMax);
