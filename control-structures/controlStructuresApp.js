708f8c67515edbdc53488176255b301fce814958
MadalinaMP <madalinapuimp@gmail.com>
2020-08-06 15:53:31 +0300

    solved 2

--- control-structures/controlStructuresApp.js
+++ control-structures/controlStructuresApp.js
@@ -3,3 +3,11 @@ console.log('1. display in the console the numbers from 1 to 20');
 for (var index = 1; index <= 20; index++) {
     console.log(index);
 }
+
+console.log('2. display in the console the odd numbers from 1 to 20');
+
+for (var index = 1; index <= 20; index++) {
+    if ((index % 2) !== 0) {
+        console.log(index);
+    }
+}
